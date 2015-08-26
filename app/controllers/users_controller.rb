1a9d99b725a5ff7eb3f449c7bc09cab4fabe82d5
Sebastian Abondano <sabondano1@gmail.com>
2015-08-26 16:51:35 -0600

    Add uniq to @orders on UsersController #show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @auctions = current_user.auctions
+    @auctions = current_user.auctions.uniq
   end
 
   def feed
--- spec/features/registered_user_views_profile_spec.rb
+++ spec/features/registered_user_views_profile_spec.rb
@@ -52,6 +52,8 @@ feature "Registered user can view user profile" do
     @auction.bids.create(amount: 16.00,
                          user_id: user2.id)
 
+    @auction.bids.create(amount: 18.00,
+                         user_id: user1.id)
   end
 
   scenario "User visits profile and sees all User info" do
@@ -80,7 +82,7 @@ feature "Registered user can view user profile" do
 
   scenario "User visists profile and sees auction history" do
     visit profile_path
-
+    save_and_open_page
     within('tr', text: "# #{@auction.id}") do
       expect(page).to have_content("The Amazing Spider-Man")
       expect(page).to have_content("July  7, 2015 at  9:33 PM")
