4f4586c30140234e9e71a3dc0bad8b7eafa4f606
MaxMillington <maxmillington89@gmail.com>
2015-08-30 17:26:06 -0600

    added more permissions tests. Having trouble with the registered user test

--- spec/features/permissions_spec.rb
+++ spec/features/permissions_spec.rb
@@ -28,17 +28,17 @@ feature "permissions_test" do
                                 last_name: "Davis",
                                 password: "password")
 
-    auction_1 = Auction.create(product_id: @product.id,
-                               starting_time: DateTime.now,
-                               ending_time: DateTime.now + 2.days,
-                               starting_price: 500,
-                               category_id: category.id)
+    @auction_1 = Auction.create(product_id: @product.id,
+                                starting_time: DateTime.now,
+                                ending_time: DateTime.now + 2.days,
+                                starting_price: 500,
+                                category_id: category.id)
 
-    auction_2 = Auction.create(product_id: @product2.id,
-                               starting_time: DateTime.now,
-                               ending_time: DateTime.now + 2.days,
-                               starting_price: 600,
-                               category_id: category2.id)
+    @auction_2 = Auction.create(product_id: @product2.id,
+                                starting_time: DateTime.now,
+                                ending_time: DateTime.now + 2.days,
+                                starting_price: 600,
+                                category_id: category2.id)
 
     Role.create(name: "platform_admin")
     Role.create(name: "seller_admin")
@@ -60,142 +60,111 @@ feature "permissions_test" do
   context "guest user can only view certain paths" do
 
     scenario "guest user can view his permissions" do
-
       visit root_path
-
       expect(current_path).to eq(root_path)
 
       click_link_or_button("Create Account")
-
       expect(current_path).to eq(profile_new_path)
 
       click_link_or_button("Sellers")
-
       expect(current_path).to eq(sellers_path)
 
       click_link_or_button("View Auctions")
-
       expect(current_path).to eq(seller_path(@seller))
 
       visit categories_path
-
       expect(current_path).to eq(categories_path)
 
     end
 
     scenario "guest user can't what he doesn't have permission for" do
       visit products_path
-
       expect(current_path).to eq(root_path)
 
     end
   end
 
-  #
-  # context "registered user can only view certain paths" do
-  #
-  #   scenario "registered user can view his permissions" do
-  #     allow_any_instance_of(ApplicationController)
-  #         .to receive(:current_user).and_return(@regular_user)
-  #     visit products_path
-  #
-  #     expect(current_path).to eq(products_path)
-  #
-  #     expect(page).to have_content("Gibson Les Paul")
-  #
-  #     expect(page).to have_content("Instruments")
-  #
-  #     within(".list-group", text: "Instruments") do
-  #       click_link_or_button("Instruments")
-  #     end
-  #
-  #     expect(page).to have_content("Gibson Les Paul")
-  #
-  #     within(".list-group", text: "Comics") do
-  #       click_link_or_button("Comics")
-  #     end
-  #
-  #     expect(page).to have_content("The Amazing Spider-Man")
-  #   end
-  #
-  #   scenario "registered user can't view what he doesn't have permission for" do
-  #     allow_any_instance_of(ApplicationController)
-  #         .to receive(:current_user).and_return(@regular_user)
-  #
-  #     visit products_path
-  #
-  #     expect(current_path).to eq(products_path)
-  #
-  #     expect(page).to have_content("Gibson Les Paul")
-  #
-  #     expect(page).to have_content("Instruments")
-  #
-  #     within(".list-group", text: "Instruments") do
-  #       click_link_or_button("Instruments")
-  #     end
-  #
-  #     expect(page).to have_content("Gibson Les Paul")
-  #
-  #     within(".list-group", text: "Comics") do
-  #       click_link_or_button("Comics")
-  #     end
-  #
-  #     expect(page).to have_content("The Amazing Spider-Man")
-  #   end
-  # end
-  #
-  # context "seller admin can only view certain paths" do
-  #
-  #   scenario "seller admin can view his permissions" do
-  #     allow_any_instance_of(ApplicationController)
-  #         .to receive(:current_user).and_return(@seller_admin)
-  #
-  #     visit products_path
-  #
-  #     expect(current_path).to eq(products_path)
-  #
-  #     expect(page).to have_content("Gibson Les Paul")
-  #
-  #     expect(page).to have_content("Instruments")
-  #
-  #     within(".list-group", text: "Instruments") do
-  #       click_link_or_button("Instruments")
-  #     end
-  #
-  #     expect(page).to have_content("Gibson Les Paul")
-  #
-  #     within(".list-group", text: "Comics") do
-  #       click_link_or_button("Comics")
-  #     end
-  #
-  #     expect(page).to have_content("The Amazing Spider-Man")
-  #   end
-  #
-  #   scenario "seller admin can't view what he doesn't have permission for" do
-  #     allow_any_instance_of(ApplicationController)
-  #         .to receive(:current_user).and_return(@seller_admin)
-  #
-  #     visit products_path
-  #
-  #     expect(current_path).to eq(products_path)
-  #
-  #     expect(page).to have_content("Gibson Les Paul")
-  #
-  #     expect(page).to have_content("Instruments")
-  #
-  #     within(".list-group", text: "Instruments") do
-  #       click_link_or_button("Instruments")
-  #     end
-  #
-  #     expect(page).to have_content("Gibson Les Paul")
-  #
-  #     within(".list-group", text: "Comics") do
-  #       click_link_or_button("Comics")
-  #     end
-  #
-  #     expect(page).to have_content("The Amazing Spider-Man")
-  #   end
-  # end
+
+  context "registered user can only view certain paths" do
+
+    scenario "registered user can view his permissions" do
+      allow_any_instance_of(ApplicationController)
+          .to receive(:current_user).and_return(@regular_user)
+
+      visit root_path
+      expect(current_path).to eq(root_path)
+
+      expect(page).to have_content("Miles Davis")
+
+      # click_link_or_button("Profile")
+      # expect(current_path).to eq(profile_path)
+
+      # visit my_bids_path
+      # expect(current_path).to eq(my_bids_path)
+
+      click_link_or_button("Auctions")
+      expect(current_path).to eq(auctions_path)
+
+      visit auction_path(@auction_1)
+      expect(current_path).to eq(auction_path(@auction_1))
+
+      visit categories_path
+      expect(current_path).to eq(categories_path)
+
+      click_link_or_button("Sellers")
+      expect(current_path).to eq(sellers_path)
+
+    end
+
+    scenario "registered user can't view what he doesn't have permission for" do
+      allow_any_instance_of(ApplicationController)
+          .to receive(:current_user).and_return(@regular_user)
+
+      visit products_path
+      expect(current_path).to eq(root_path)
+
+    end
+  end
+
+  context "seller admin can only view certain paths" do
+
+    scenario "seller admin can view his permissions" do
+      allow_any_instance_of(ApplicationController)
+          .to receive(:current_user).and_return(@seller_admin)
+
+      visit root_path
+      expect(current_path).to eq(root_path)
+
+      click_link_or_button("Auctions")
+      expect(current_path).to eq(auctions_path)
+
+      visit auction_path(@auction_1)
+      expect(current_path).to eq(auction_path(@auction_1))
+
+      visit categories_path
+      expect(current_path).to eq(categories_path)
+
+      visit root_path
+      click_link_or_button("Logout")
+      expect(current_path).to eq(root_path)
+
+      click_link_or_button("Seller Dashboard")
+      expect(current_path).to eq("/acme/dashboard")
+
+      click_link_or_button("Sellers")
+      expect(current_path).to eq(sellers_path)
+
+    end
+
+    scenario "seller admin can't view what he doesn't have permission for" do
+      allow_any_instance_of(ApplicationController)
+          .to receive(:current_user).and_return(@seller_admin)
+
+      visit products_path
+      expect(current_path).to eq(root_path)
+
+    end
+  end
   #
   # context "platform admin can see all" do
   #
