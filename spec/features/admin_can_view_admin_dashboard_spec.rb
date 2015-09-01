e5795f0e6e5e5ecbb7cfa6401ace28d7ab42c869
MaxMillington <maxmillington89@gmail.com>
2015-09-01 08:13:35 -0600

    got the admin visits admin path test to pass

--- spec/features/admin_can_view_admin_dashboard_spec.rb
+++ spec/features/admin_can_view_admin_dashboard_spec.rb
@@ -2,22 +2,30 @@ require "rails_helper"
 
 feature "Admin can view Admin Dashboard" do
   scenario "Admin logs in and sees Admin Dashboard for /admin/dashboard" do
-    User.create(first_name: "Admin",
-                last_name: "Admin",
-                email: "admin@admin.com",
-                password: "password",
-                role: 1)
+    @seller = Seller.create(name: "ACME",
+                            email: "hello@acme.com")
+
+    seller_admin = User.create(first_name: "John",
+                               last_name: "Doe",
+                               email: "email@example.com",
+                               password: "password",
+                               seller_id: @seller.id)
+
+    Role.create(name: "platform_admin")
+    Role.create(name: "seller_admin")
+    Role.create(name: "registered_user")
+
+    seller_admin.roles << Role.find_by(name: "seller_admin")
 
     visit login_path
 
-    fill_in "Email", with: "admin@admin.com"
+    fill_in "Email", with: "email@example.com"
     fill_in "Password", with: "password"
     click_button "Login"
 
-    visit admin_dashboard_path
+    visit seller_dashboard_path(@seller.slug)
 
-    expect(current_path).to eq("/admin/dashboard")
-    expect(page).to have_content("Admin Dashboard")
+    expect(current_path).to eq("/acme/dashboard")
   end
 
   scenario "Non-admin logs in and sees 404 page for /admin/dashboard" do
