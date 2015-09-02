49aff81c4bb6b1aff9d3d3c5d70482c7571bc384
MaxMillington <maxmillington89@gmail.com>
2015-09-02 09:20:36 -0600

    platform admin can successfully delete seller

--- app/controllers/platform_admin/sellers_controller.rb
+++ app/controllers/platform_admin/sellers_controller.rb
@@ -9,12 +9,18 @@ class PlatformAdmin::SellersController < ApplicationController
 
     if @seller.save
       flash[:success] = "Successfully created new seller."
-      redirect_to seller_dashboard_path(@seller.slug)
+      redirect_to platform_admin_dashboard_path
     else
       flash.now[:warning] = @seller.errors.full_messages.join(". ")
       render :new
     end
+  end
 
+  def destroy
+    @seller = Seller.find(params[:id])
+    @seller.destroy
+    flash[:success] = "Successfully deleted seller."
+    redirect_to platform_admin_dashboard_path
   end
 
   private
