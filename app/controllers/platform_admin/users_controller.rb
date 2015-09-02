d29ed7e0a38a7b3e75d2ce9e9de8db9c8f04a7c4
MaxMillington <maxmillington89@gmail.com>
2015-09-02 10:53:48 -0600

    got it working so that a platform admin can create a seller admin

--- app/controllers/platform_admin/users_controller.rb
+++ app/controllers/platform_admin/users_controller.rb
@@ -24,5 +24,12 @@ class PlatformAdmin::UsersController < ApplicationController
     end
   end
 
+  private
+
+  def user_params
+    params.require(:user)
+        .permit(:first_name, :last_name,
+                :email, :password, :seller_id)
+  end
 
 end
