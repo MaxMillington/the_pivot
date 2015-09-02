816ae4b7fe7123ad9e1fe07793ca34afcae9aa88
MaxMillington <maxmillington89@gmail.com>
2015-09-02 09:23:03 -0600

    added dependent destroy to the sellers products and auctions so that if someone deletes a seller all the products go with it

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -1,7 +1,7 @@
 class Seller < ActiveRecord::Base
   has_many :addresses, as: :addressable
-  has_many :products
-  has_many :auctions, through: :products
+  has_many :products, dependent: :destroy
+  has_many :auctions, through: :products, dependent: :destroy
   has_many :users
 
   before_validation :strip_whitespace
