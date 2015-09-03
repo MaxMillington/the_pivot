d20f7e07c5f3129f6535ef16e782789db5d9cfc8
Sebastian Abondano <sabondano1@gmail.com>
2015-09-03 11:46:18 -0600

    Fix bug in seller_notifier and user_notifier

--- app/worker/seller_notifier.rb
+++ app/worker/seller_notifier.rb
@@ -1,7 +1,8 @@
 class SellerNotifier
   include Sidekiq::Worker
 
-  def perform(seller)
+  def perform(seller_id)
+    seller = Seller.find(seller_id)
     SellerMailer.send_auction_over_email(seller).deliver_now
   end
 end
--- app/worker/user_notifier.rb
+++ app/worker/user_notifier.rb
@@ -1,7 +1,8 @@
 class UserNotifier
   include Sidekiq::Worker
 
-  def perform(auction)
+  def perform(auction_id)
+    auction = Auction.find(aution_id)
     UserMailer.win_bid(auction.winner).deliver_now
   end
 end
