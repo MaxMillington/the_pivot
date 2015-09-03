47a36baf182e719f68c6d3708b09d22f968a382a
Sebastian Abondano <sabondano1@gmail.com>
2015-09-03 11:51:30 -0600

    Fix type in worker

--- app/worker/user_notifier.rb
+++ app/worker/user_notifier.rb
@@ -2,7 +2,7 @@ class UserNotifier
   include Sidekiq::Worker
 
   def perform(auction_id)
-    auction = Auction.find(aution_id)
+    auction = Auction.find(auction_id)
     UserMailer.win_bid(auction.winner).deliver_now
   end
 end
